6f3a5c3e0de1cd4626c1507ca03566bac3f37bdf
張誌叡 <ray_chang@mayohr.com>
2022-11-12 00:32:23 +0800

    update firm section

--- src/components/firm-section/index.jsx
+++ src/components/firm-section/index.jsx
@@ -9,11 +9,21 @@ import { ReactComponent as TitanLogo } from '@/assets/titan-logo.svg';
 import { ReactComponent as Diamondlg } from '@/assets/diamond-lg.svg';
 import { ReactComponent as Diamondmd } from '@/assets/diamond-md.svg';
 import { ReactComponent as Diamondsm } from '@/assets/diamond-sm.svg';
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useInView } from "framer-motion";
 
-function getRandom(x){
-  return Math.floor(Math.random()*x);
+function getRandom(x, excludes = []){
+  let num = Math.floor(Math.random()*x);
+  while (excludes.includes(num) && num === 0) {
+    num = Math.floor(Math.random()*x)
+  }
+  return num;
 };
+
+function removeItem(arr, item) {
+  const index = arr.indexOf(item);
+  if (index > -1) arr.splice(index, 1);
+}
+
 
 const FirmBlock = ({ title, firmLogos }) => {
   return (
@@ -23,7 +33,9 @@ const FirmBlock = ({ title, firmLogos }) => {
         {
           firmLogos.map((logo, idx) => {
             return (
-              <div key={`firmlogo_${idx}`} className="w-[202px] h-[202px] sm:w-[432px] sm:h-[432px] md:w-[374px] md:h-[374px] bg-white flex justify-center items-center p-[5%]">
+              <div key={`firmlogo_${idx}`} className="w-[202px] h-[202px] sm:w-[432px] sm:h-[432px] md:w-[374px] md:h-[374px] bg-white flex justify-center items-center p-[5%] relative">
+                <div className="z-20 bg-white absolute w-full h-full">
+                </div>
                 {logo}
               </div>
             )
@@ -33,40 +45,93 @@ const FirmBlock = ({ title, firmLogos }) => {
     </div>
   )
 }
+
+const FloatingDiamonds = ({ quantity, height, width, size = 'sm', start }) => {
+  if (height <= 0 || !start) {
+    return <></>
+  }
+  const speed = size === 'sm' ? 3 : size === 'md' ? 5 : size === 'lg' ? 8 : 3;
+  const zIndex = size === 'sm' ? 10 : size === 'md' ? 30 : size === 'lg' ? 50 : 10;
+
+  const [items, setItems] = useState(new Array(quantity).fill().map(() => getRandom(height)))
+  return (
+    <AnimatePresence mode={"popLayout"}>
+      {items.map((y, idx) => (
+        <motion.div
+          layout
+          key={`${size}-${y}`}
+          animate={{
+            x: width + 50,
+          }}
+          initial={{ x: '-100%', y }}
+          style={{
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            zIndex,
+          }}
+          onAnimationComplete={() => {
+            const newItems = [...items, getRandom(height, items)];
+            removeItem(newItems, y);
+            setItems(newItems);
+          }}
+          transition={{ ease: "linear", duration: speed, delay: Math.random() * (idx + 1) }}
+        >
+          { size === 'lg' && <Diamondlg /> }
+          { size === 'md' && <Diamondmd /> }
+          { size === 'sm' && <Diamondsm /> }
+        </motion.div>
+      ))}
+    </AnimatePresence>
+  )
+}
 const FirmSection = () => {
   const container = useRef();
-  console.log(window.innerHeight, window.innerWidth)
+  const isInView = useInView(container)
+  const [height, setHeight] = useState(null);
+  useEffect(() => {
+    setHeight(container.current.offsetHeight);
+  }, []);
+
   return (
-    <div ref={container} className="w-full bg-primary py-[100px] px-[35px] relative">
+    <div ref={container} className="w-full bg-primary py-[100px] px-[35px] relative overflow-hidden">
       <FirmBlock
         title={<span><span className="text-yellow">鑽石級</span>贊助商</span>}
         firmLogos={[
-          <BlockStudioLogo className="w-full h-auto"/>,
-          <TitanLogo className="w-full h-auto"/>,
-          <KdanLogo className="w-full h-auto"/>,
+          <BlockStudioLogo className="w-full h-auto z-40"/>,
+          <TitanLogo className="w-full h-auto z-40"/>,
+          <KdanLogo className="w-full h-auto z-40"/>,
         ]}
       />
       <FirmBlock
         title={"共同推廣"}
         firmLogos={[
-          <JiraLogo className="w-full h-auto"/>,
-          <MiroLogo className="w-full h-auto"/>,
-          <DottedSignLogo className="w-full h-auto"/>,
+          <JiraLogo className="w-full h-auto z-40"/>,
+          <MiroLogo className="w-full h-auto z-40"/>,
+          <DottedSignLogo className="w-full h-auto z-40"/>,
         ]}
       />
-      {/* <motion.div
-        animate={{
-          left: window.innerWidth,
-          position: 'absolute',
-        }}
-        style={{
-          top: getRandom(window.innerHeight),
-          left: 0,
-        }}
-        transition={{ repeat: Infinity, duration: 2 }}
-      >
-        <Diamondlg/>
-      </motion.div> */}
+      <FloatingDiamonds
+        start={isInView}
+        size={'lg'}
+        quantity={4}
+        width={window.innerWidth}
+        height={height}
+      />
+      <FloatingDiamonds
+        start={isInView}
+        size={'md'}
+        quantity={6}
+        width={window.innerWidth}
+        height={height}
+      />
+      <FloatingDiamonds
+        start={isInView}
+        size={'sm'}
+        quantity={8}
+        width={window.innerWidth}
+        height={height}
+      />
     </div>
   )
 }
